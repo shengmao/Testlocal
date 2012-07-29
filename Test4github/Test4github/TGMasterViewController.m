1430d61ec5e7caeb5dd8136a98e3dfc266bc741b
shengmao <shengmao.li@techedgegroup.com>
2012-07-29 15:53:32 +0200

    commit after adding two perfect methods

--- Test4github/Test4github/TGMasterViewController.m
+++ Test4github/Test4github/TGMasterViewController.m
@@ -117,7 +117,8 @@
     self.detailViewController.detailItem = object;
 }
 -(void) prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
-    
+    NSLog(@"---> %s",__PRETTY_FUNCTION__);
+    NSLog(@"---> %s",__PRETTY_FUNCTION__);
 }
 
 @end
