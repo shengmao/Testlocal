9ebd039167e8a671adecd76db189f1a71cc61b97
shengmao <shengmao.li@techedgegroup.com>
2012-07-29 15:07:34 +0200

    *1commit

---
+++ Test4github/Test4github/TGMasterViewController.h
@@ -0,0 +1,17 @@
+//
+//  TGMasterViewController.h
+//  Test4github
+//
+//  Created by LISComputer on 27.07.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class TGDetailViewController;
+
+@interface TGMasterViewController : UITableViewController
+
+@property (strong, nonatomic) TGDetailViewController *detailViewController;
+
+@end
