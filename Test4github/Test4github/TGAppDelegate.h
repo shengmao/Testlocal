99ad257f70c259146f12e42a06c0a4b65cb9cf1a
shengmao <shengmao.li@techedgegroup.com>
2012-07-29 14:36:11 +0200

    ***commit

---
+++ Test4github/Test4github/TGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TGAppDelegate.h
+//  Test4github
+//
+//  Created by LISComputer on 27.07.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Test4github/Test4github/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Test4github
+//
+//  Created by LISComputer on 27.07.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TGAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TGAppDelegate class]));
+    }
+}
